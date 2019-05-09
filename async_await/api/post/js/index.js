3c55f7d4e27c7cbb4ab1a9be4cc39393c6694428
saniag081 <sgonzalezhernandez081@gmail.com>
2019-05-08 20:21:03 -0500

    async await Api and POST

--- async_await/api/post/js/index.js
+++ async_await/api/post/js/index.js
@@ -1,18 +1,18 @@
-let formulario  = document.getElementById('form'); 
+let formulario = document.getElementById('form');
 let btn = document.getElementById('btn');
 let parrafo = document.getElementById('parrafo');
 
 formulario.addEventListener('submit', post)
 
 async function post(e) {
-    try{ 
+    try {
         e.preventDefault();
-        
+
         let data = new FormData(formulario);
-         
+
         console.log(data.get('pass'));
 
-        let enviar = await fetch('js/post.php',{
+        let enviar = await fetch('js/post.php', {
 
             method: 'POST',
 
@@ -20,21 +20,21 @@ async function post(e) {
 
         })
 
-            let res = await enviar.json();
+        let res = await enviar.json();
 
-            // console.log(res);
-            // parrafo.textContent = `${res}`;
-            parrafo.innerHTML = `
+        // console.log(res);
+        // parrafo.textContent = `${res}`;
+        parrafo.innerHTML = `
             
             <p>${res}</p>
 
             `
 
-        }catch(erro){
+    } catch (erro) {
 
-            console.log(erro);
-
-        }
+        console.log(erro);
 
     }
+
+}
 
